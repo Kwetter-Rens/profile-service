6f38b1ab8bacffc6de173e41ac3a07127405c194
Rens Kloosterman <rens.kloosterman@student.fontys.nl>
2022-12-09 16:08:09 +0100

    cahnged endpoint path

--- src/main/java/com/fhict/profileservice/controllers/ProfileController.java
+++ src/main/java/com/fhict/profileservice/controllers/ProfileController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/profile")
+@RequestMapping("/api/v1/profile")
 public class ProfileController {
     private final ProfileService profileService;
     private final RabbitTemplate rabbitTemplate;

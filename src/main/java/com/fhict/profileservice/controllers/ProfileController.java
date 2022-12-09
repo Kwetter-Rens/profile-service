03d7dd2516aa3b793ca6c7bf0df4aefdfa6963d0
Rens Kloosterman <rens.kloosterman@student.fontys.nl>
2022-12-09 15:07:23 +0100

    INIT

---
+++ src/main/java/com/fhict/profileservice/controllers/ProfileController.java
@@ -0,0 +1,50 @@
+package com.fhict.profileservice.controllers;
+
+import com.fhict.profileservice.models.DTO.ProfileWithFollowing;
+import com.fhict.profileservice.models.Profile;
+import com.fhict.profileservice.services.ProfileService;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/profile")
+public class ProfileController {
+    private final ProfileService profileService;
+    private final RabbitTemplate rabbitTemplate;
+
+    public ProfileController(ProfileService profileService, RabbitTemplate rabbitTemplate) {
+        this.profileService = profileService;
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    @GetMapping("/getall")
+    public List<Profile> getAllProfiles() {
+        return this.profileService.getAll();
+    }
+
+    @GetMapping("/{username}")
+    public ResponseEntity<ProfileWithFollowing> getProfileByUsername(@PathVariable String username) {
+        Profile profile = profileService.getProfileByUsername(username);
+        if(profile == null) {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+        List<String> followingUserIds = (List<String>) rabbitTemplate.convertSendAndReceive("kwetter", "following", profile.getUserId());
+        ProfileWithFollowing profileWithFollowing = new ProfileWithFollowing(profile, followingUserIds);
+        return new ResponseEntity<>(profileWithFollowing, HttpStatus.OK);
+    }
+
+    @PostMapping("/createprofile")
+    public ResponseEntity<Profile> createProfile(@RequestBody Profile profile) {
+        System.out.println("createprofile controller");
+        return profileService.createProfile(profile);
+    }
+
+    @PutMapping("/updateprofile")
+    public ResponseEntity<Void> updateProfile(@RequestBody Profile profile) {
+        return profileService.updateProfile(profile);
+    }
+}
